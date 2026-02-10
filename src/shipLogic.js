bc26485d202ccb332893f0064e78892f0ab91386
Carina Diesel <carinadiesel@OKDVTL7V92K4.local>
2026-02-10 17:36:29 +0200

    Added ship logic

---
+++ src/shipLogic.js
@@ -0,0 +1,30 @@
+
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.positions = []; 
+    this.placed = false;
+    this.hits = 0;
+  }
+
+  place(startRow, startCol, direction) {
+    this.positions = [];
+    for (let counter = 0; counter < this.length; counter++) {
+      if (direction === "horizontal") {
+        this.positions.push({ row: startRow, col: startCol + counter });
+      } else {
+        this.positions.push({ row: startRow + counter, col: startCol });
+      }
+    }
+    this.placed = true;
+  }
+  // Logic for enemy return fire
+  hit() {
+    this.hits++;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
